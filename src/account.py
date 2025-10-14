52cda2413a9b2c1cfb4b94666445d9915cc43742
@blazepiec <blazepiec@gmail.com>
2025-10-14 15:59:07 +0200

    feature4

--- src/account.py
+++ src/account.py
@@ -1,12 +1,21 @@
 class Account:
-    def __init__(self, first_name, last_name, pesel):
+    def __init__(self, first_name, last_name, pesel, promocode=None):
         self.first_name = first_name
         self.last_name = last_name
         self.balance = 0
         self.pesel = pesel if self.is_pesel_valid(pesel) else "Invalid"
-    
+        self.promocode = promocode if self.is_promocode_valid(promocode) else 0.0
+        
+        
     def is_pesel_valid(self, pesel):
         if len(pesel) == 11 and pesel.isdigit():
             return True
         return False
     
+    def is_promocode_valid(self, promocode):
+        if promocode is None:
+            return True
+        if promocode.startswith("PROM_") and len(promocode) == 8:
+            self.balance += 50
+            return True
+        return False
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -8,6 +8,7 @@ class TestAccount:
         assert account.last_name == "Doe"
         assert account.balance == 0
         assert account.pesel == "12345678911"
+        assert account.promocode is None
         
     def test_pesel_length(self):
         account = Account("John", "Doe", "12345678911")
@@ -24,3 +25,23 @@ class TestAccount:
     def test_pesel_non_numeric(self):
         account = Account("John", "Doe", "12345ABCDE1")
         assert account.pesel == "Invalid"
+        
+    def test_promocode_valid(self):
+        account = Account("John", "Doe", "12345678911", promocode="PROM_123")
+        assert account.balance == 50
+    
+    def test_promocode_none(self):
+        account = Account("John", "Doe", "12345678911", promocode=None)
+        assert account.balance == 0
+
+    def test_promocode_invalid_format(self):
+        account = Account("John", "Doe", "12345678911", promocode="PROMO123")
+        assert account.balance == 0
+
+    def test_promocode_invalid_prefix(self):
+        account = Account("John", "Doe", "12345678911", promocode="INVALID_123")
+        assert account.balance == 0
+        
+    def test_promocode_invalid_suffix(self):
+        account = Account("John", "Doe", "12345678911", promocode="PROM_9992")
+        assert account.balance == 0

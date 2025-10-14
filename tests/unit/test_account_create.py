d370fcf808c04ad9aa39d6582f711fb03989caa2
@blazepiec <blazepiec@gmail.com>
2025-10-14 15:38:04 +0200

    feature3

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -8,3 +8,19 @@ class TestAccount:
         assert account.last_name == "Doe"
         assert account.balance == 0
         assert account.pesel == "12345678911"
+        
+    def test_pesel_length(self):
+        account = Account("John", "Doe", "12345678911")
+        assert account.pesel == "12345678911"
+        
+    def test_pesel_too_long(self):
+        account = Account("John", "Doe", "123456789112")
+        assert account.pesel == "Invalid"
+    
+    def test_pesel_too_long(self):
+        account = Account("John", "Doe", "1234567891")
+        assert account.pesel == "Invalid"
+    
+    def test_pesel_non_numeric(self):
+        account = Account("John", "Doe", "12345ABCDE1")
+        assert account.pesel == "Invalid"
